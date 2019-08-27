efe7e66d74381ffcc3b2b910b301ead1b444ebc8
Santiago Enciso <whereispolaris@gmail.com>
2019-08-26 23:11:04 -0500

    Created Header component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App jumbotron">
-      <h1>This is the header</h1>
-    </div>
+    <Header />
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-sm">
+                    One of three columns
+                </div>
+                <div className="col-sm">
+                    One of three columns
+                </div>
+                <div className="col-sm">
+                    One of three columns
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
