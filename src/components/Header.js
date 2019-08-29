d7055a5b5dc74dd124c9690ed4951191f16647d4
Santiago Enciso <whereispolaris@gmail.com>
2019-08-29 18:30:25 -0500

    Added CSS file to Header component, added comments to break down Header contents into 3 child components.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import style from './Header.css'
 
 function Header() {
     return (
         <div className="container">
             <div className="row">
+                // Home Component
                 <div className="col-sm">
                     One of three columns
                 </div>
+                // Status Component
                 <div className="col-sm">
                     One of three columns
                 </div>
+                // Score Component
                 <div className="col-sm">
                     One of three columns
                 </div>
