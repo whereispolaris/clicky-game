f40496bda37bbd472d645e46b22e92f415028a41
Santiago Enciso <whereispolaris@gmail.com>
2019-08-31 12:45:51 -0500

    Updates

---
+++ precache-manifest.c357f9424a6a9fa42d0ed98640ac15b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8762da52d9b9a8bd786f9cb8ef032523",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "b93c835cfe05f18bb312",
+    "url": "/clicky-game/static/css/main.0dcde870.chunk.css"
+  },
+  {
+    "revision": "3c07a65b0e1d9963a428",
+    "url": "/clicky-game/static/js/2.1a2d5527.chunk.js"
+  },
+  {
+    "revision": "b93c835cfe05f18bb312",
+    "url": "/clicky-game/static/js/main.1d7505dc.chunk.js"
+  },
+  {
+    "revision": "79ea0d5502c3a6ac0f74",
+    "url": "/clicky-game/static/js/runtime~main.80bf01e5.js"
+  }
+]);
