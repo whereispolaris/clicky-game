3dddcdacf49c0b6824a1b880dc26778f5374d4e3
Santiago Enciso <whereispolaris@gmail.com>
2019-09-04 21:48:20 -0500

    Updated jumbotron with image

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Jumbotron() {
     return (
-        <div className="jumbotron jumbotron-fluid">
-            <div className="container-fluid">
-                <h1 className="display-4">Clicky Game!</h1>
+        <div className="jumbotron jumbotron-fluid text-center">
+            <div className="container-fluid justify-items-center">
+                <img src="https://whereispolaris.github.io/clicky-game/img/guessWhoLogo.png" alt="guessWho" />
                 <p className="lead">Click on an image to earn points, but don't click on any more than once!</p>
             </div>
         </div>
