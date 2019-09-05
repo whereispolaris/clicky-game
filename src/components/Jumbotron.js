18198886f114d821b9b1866fb1a11c2004b66915
Santiago Enciso <whereispolaris@gmail.com>
2019-09-05 18:20:18 -0500

    Added top padding in the main logo to keep it from touching the header.

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Jumbotron.css';
 
 function Jumbotron() {
     return (
         <div className="jumbotron jumbotron-fluid text-center">
             <div className="container-fluid justify-items-center">
-                <img src="https://whereispolaris.github.io/clicky-game/img/guessWhoLogo.png" alt="guessWho" />
+                <img className="main-logo" src="https://whereispolaris.github.io/clicky-game/img/guessWhoLogo.png" alt="guessWho" />
                 <h4 className="instructions">Click on an image to earn points, but don't click on any more than once!</h4>
             </div>
         </div>
