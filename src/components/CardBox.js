c07e05b33a6aa040bde993d922243dd2e3557210
Santiago Enciso <whereispolaris@gmail.com>
2019-09-06 12:31:33 -0500

    Added animation keyframes to shake entire CardBox container if the user clicks on the wrong image.

--- src/components/CardBox.js
+++ src/components/CardBox.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './CardBox.css'
 
 function CardBox(props) {
     return (
         <div className={props.wrongImage ? "shake container-fluid text-center" : "container-fluid text-center"} >
-            <div className="row" style={{ margin: '20px 10px' }}>
+            <div className="row">
                 {props.children}
             </div>
         </div>
