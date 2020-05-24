33749832922dc64d9acb2f67ab01734cae624166
Santiago Enciso <whereispolaris@gmail.com>
2020-05-24 15:14:00 -0500

    Updated start message.

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
 		guessWho,
 		score: 0,
 		topScore: 0,
-		message: 'Click An Image to Begin!',
+		message: 'Click On A Character!',
 		clickedCards: [],
 		wrongImage: false,
 	};
